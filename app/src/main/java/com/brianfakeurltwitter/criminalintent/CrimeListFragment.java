001eb07bcb9dde421df5f9d3fd4444a1591e776f
brianbinder <bbinder912@gmail.com>
2019-10-03 19:12:48 -0400

    Update gitignore

--- app/src/main/java/com/brianfakeurltwitter/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/brianfakeurltwitter/criminalintent/CrimeListFragment.java
@@ -1,5 +1,6 @@
 package com.brianfakeurltwitter.criminalintent;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.VibrationEffect;
 import android.view.Gravity;
@@ -67,8 +68,8 @@ public class CrimeListFragment extends Fragment {
 
         @Override
         public void onClick(View view) {
-            Toast.makeText(getActivity(), mCrime.getTitle() + " clicked!", Toast.LENGTH_SHORT)
-                    .show();
+            Intent intent = new Intent(getActivity(), CrimeActivity.class);
+            startActivity(intent);
         }
     }
 
